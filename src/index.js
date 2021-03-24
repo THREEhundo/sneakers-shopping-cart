7ecf37cfbea758920f6d57402f97d65d4ae889ac
threehundo <samuelbaik87@gmail.com>
2021-03-24 09:44:40 -0700

    basename added to hashrouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL + "/"}>
       <App />
     </HashRouter>
   </React.StrictMode>,
